b76aeb6e3cf23c01f89af361ac7b0920f7df90ca
Jorge Vazquez <jorge@totema.co>
2024-01-23 22:52:27 +0100

    add axios setup and api integrations

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,23 +1,29 @@
 import axios from 'axios';
+import { IBook } from '../models/types';
 
-const API_URL = 'http://localhost:3001/books';
+const booksApi = axios.create({
+    baseURL: 'http://localhost:3001'
+});
+
+export const API_URL = '/books';
 
 export const fetchBooks = async () => {
-    const response = await axios.get(`${API_URL}`);
+    const response = await booksApi.get(API_URL);
     return response.data;
 };
 
-export const addBook = async (book: any) => {
-    const response = await axios.post(`${API_URL}`, book);
+export const addBook = async (book: IBook) => {
+    const response = await booksApi.post(API_URL, book);
+    return response.data;
+};
+
+export const updateBook = async (book: IBook) => {
+    const response = await booksApi.put(`${API_URL}/${book.id}`, book);
     return response.data;
 }
 
-export const updateBook = async (id: string, book: any) => { 
-    const response = await axios.put(`${API_URL}/${id}`, book);
-    return response.data;
-}
+export const removeBook = async (id: string | number) => {
+    return await booksApi.delete(`${API_URL}/${id}`);
+};
 
-export const deleteBook = async (id: string) => {
-    const response = await axios.delete(`${API_URL}/${id}`);
-    return response.data;
-}
+
